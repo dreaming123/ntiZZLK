181b7da3e0117e8f18d755cb470fb44057c92344
jcz <1146933683@qq.com>
2020-06-17 16:12:19 +0800

    测试2下

--- PROJECT/OracleTest/jcz/dao/OracleDB.cs
+++ PROJECT/OracleTest/jcz/dao/OracleDB.cs
@@ -12,7 +12,7 @@ namespace OracleTest.jcz.dao
    public  class OracleDB
     {
 
-         //123
+         //123123
         public static SqlSugarClient GetInstance()
         {
             return new SqlSugarClient(new ConnectionConfig()
